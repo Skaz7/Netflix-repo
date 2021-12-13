f9e63a74650d8b5ac350b59af6c4ed6194663732
Skaz7 <sebastian.kupis@gmail.com>
2021-12-13 22:56:00 +0100

    third commit - working functions, some errors

--- Netflix.py
+++ Netflix.py
@@ -6,31 +6,15 @@ import time
 netflix_data_file = "d:/users/sebas/onedrive/repositories/netflix-repo/ViewingActivity.xlsx"
 df = pd.read_excel(netflix_data_file)
 
-# lista_danych = df.head(50).values.tolist() # lista składająca się jedynie z pierwszych 50 pozycji
-
 df = df.drop(['Bookmark', 'Latest Bookmark', 'Country'], axis=1)
-
 
 df['Start Time'] = pd.to_datetime(df['Start Time']) # zmiana formatu na datę
 df['Duration'] = pd.to_timedelta(df['Duration']) # zmiana formatu na czas trwania
-
 
 asia = df[df['Profile Name'].str.contains('Asia', regex=False)]
 seeb = df[df['Profile Name'].str.contains('Seeb', regex=False)]
 kuba = df[df['Profile Name'].str.contains('Kuba', regex=False)]
 zetka = df[df['Profile Name'].str.contains('Zetka', regex=False)]
-
-
-# print(seeb['Duration'].sum())
-# print(kuba['Duration'].sum())
-# print(zetka['Duration'].sum())
-# print(asia['Duration'].sum())
-
-# print()
-# print(f'Asia zaczęła oglądać Netflix w dniu {min(asia["Start Time"])}')
-# print()
-# print(f'Asia ostani raz oglądała Netflix w dniu {max(asia["Start Time"])}')
-# print()
 
 
 def clear_screen():
@@ -62,14 +46,15 @@ def watching_movies_time():
         if movie_title == '0':
             general_analysis()
         else:
-            new_df = df[df["Title"].str.contains(f'{movie_title}'.title(), case=False)] # creates new dataframe named '{movie_title}'
+            new_df = df[df["Title"].str.contains(f'{movie_title}'.title(), case=False)]
             
             print()
             print(f'\n\n\t\tSumaryczny czas oglądania szukanego programów zawierających w tytule słowa "{movie_title}" wynosi:')
             print('\n\t\t===================')          
             print(f'\t\t  {new_df["Duration"].sum()}')
             print('\t\t===================')   
-            continue   
+            input('\n\n\t\tEnter - dalej')
+            watching_movies_time()   
 
 
 def device_type():
@@ -132,20 +117,96 @@ def general_analysis():
         main_screen()
 
 
-def asia_analysis():
-    pass
+def movie_time_by_user(user):
+    clear_screen()
+
+    while True:
+        print('\n\n\t\tWpisz tytuł filmu lub serialu, którego czas oglądania chcesz poznać.')
+        print(('\n\t\t0 - Powrót'))
+
+        movie_title = input('\n\t\t\tTytuł: ')
+
+        if movie_title == '0':
+            user_analysis(user)
+        else:
+            if user == 'Asia':
+                new_df = asia[df["Title"].str.contains(f'{movie_title}'.title(), case=False)]
+            elif user == 'Seeb':
+                new_df = seeb[df["Title"].str.contains(f'{movie_title}'.title(), case=False)]
+            elif user == 'Kuba':
+                new_df = kuba[df["Title"].str.contains(f'{movie_title}'.title(), case=False)]
+            elif user == 'Zetka':
+                new_df = zetka[df["Title"].str.contains(f'{movie_title}'.title(), case=False)]
+            
+            print()
+            print(f'\n\n\t\tSumaryczny czas oglądania szukanego programów zawierających w tytule słowa "{movie_title}" wynosi:')
+            print('\n\t\t===================')          
+            print(f'\t\t  {new_df["Duration"].sum()}')
+            print('\t\t===================')   
+            input('\n\n\t\t\tEnter - dalej.')
+            movie_time_by_user(user)
 
 
-def kuba_analysis():
-    pass
+def device_type_by_user(user):
+    clear_screen()
+
+    print(f'\n\n\t\tSumaryczny czas oglądania Netflixa na poszczególnych urządzeniach przez użytkownika {user}.')
+    print('\n\t\tW czasie między 29.10.2017 a 06.11.2021 do oglądania wykorzystano poniższe urządzenia:\n\n')
+
+    device_type_list = list(df['Device Type'].drop_duplicates())
+    
+    for i in range(len(device_type_list)):
+
+        device_df = df[df['Profile Name'].str.contains(f'{user}', regex=False)][df['Device Type'].str.contains(f'{device_type_list[i]}', regex=False)]
+        print(f'\t\t{device_df["Duration"].sum()} -  {device_type_list[i]}')
+    
+    input('\n\nEnter - Dalej')
+    user_analysis(user)
 
 
-def seeb_analysis():
-    pass
+def most_least_watched(user):
+    clear_screen()
+    
+    print(f'\n\n\t\tNajdłużej i najkrócej oglądane programy przez użytkownika {user}:')
+
+    # can't create function - too many title differences, different seasons, different episode titles etc.
+    
+    input('\n\nEnter - Dalej')
+    user_analysis(user)
 
 
-def zuzia_analysis():
-    pass
+def user_analysis(user):
+    clear_screen()
+
+    print(f'''
+                        =====================
+                          Netflix - {user}  
+                        =====================
+    ''')
+
+    profile_total_duration = df[df['Profile Name'].str.contains(f'{user}', regex=False)]['Duration'].sum()
+
+    print(f'\n\n\t\tCałkowity czas oglądania Netflix przez {user}:  {profile_total_duration}')
+    print('\n\t\tDostępne informacje:')
+    print('\n\t\t\t1 - Czas oglądania wybranego programu.')
+    print('\t\t\t2 - Czas oglądania na poszczególnych urządzeniach.')
+    print('\t\t\t3 - Najdłużej i najkrócej oglądane programy.')
+    print('\n\t\t\t0 - Powrót')
+
+    choice = input('\n\n\t\tTwój wybór :')
+
+    if choice == '0':
+        main_screen()
+    elif choice == '1':
+        movie_time_by_user(user)
+    elif choice == '2':
+        device_type_by_user(user)
+    elif choice == '3':
+        most_least_watched(user)
+    else:
+        print('\n\n\t\t\tBłędny wybór!')
+        time.sleep(1)
+        user_analysis(user)
 
 
 def main_screen():
@@ -167,16 +228,16 @@ def main_screen():
         general_analysis()
 
     elif choice == '2':
-        asia_analysis()
+        user_analysis('Asia')
 
     elif choice == '3':
-        kuba_analysis()
+        user_analysis('Kuba')
 
     elif choice == '4':
-        seeb_analysis()
+        user_analysis('Seeb')
 
     elif choice == '5':
-        zuzia_analysis()
+        user_analysis('Zetka')
     
     elif choice == '0':
         print('\n\n\t\t\tDO ZOBACZENIA!\n\n')
